c0b10be1f0b57c1c9c677e3093ce00941b91691c
leroy <theclosingbellclub@gmail.com>
2025-05-24 15:16:12 -0700

    Enable routing and add /privacy route

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import './App.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App.jsx";
+import PrivacyPolicy from "./components/PrivacyPolicy.jsx"; // Make sure this path is correct
+import "./index.css";
+import "./App.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/privacy" element={<PrivacyPolicy />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
