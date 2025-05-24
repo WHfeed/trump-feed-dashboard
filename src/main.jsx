b24d4a013720ad9d431f641abba7d8120190e29a
leroy <theclosingbellclub@gmail.com>
2025-05-24 16:41:19 -0700

    updates main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
-import PrivacyPolicy from "./components/PrivacyPolicy.jsx"; // Make sure this path is correct
+import PrivacyPolicy from "./components/PrivacyModal.jsx"; // Make sure this path is correct
 import "./index.css";
 import "./App.css";
 
